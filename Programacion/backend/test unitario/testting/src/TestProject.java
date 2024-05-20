7f3c4c2906c4b3da70f5974b5338157f8a7284f8
Roberto-Giler <gilerroberto426@gmail.com>
2024-05-19 20:27:12 -0500

    guardado semanal

---
+++ Programacion/backend/test unitario/testting/src/TestProject.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestProject {
+    @Test
+    void testearQueSeanIguales(){
+        int numero_1=2;
+        int numero_2=2;
+
+        assertEquals(numero_1,numero_2);
+    }
+
+}
